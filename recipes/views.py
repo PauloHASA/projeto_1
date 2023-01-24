66de3005679bbb9af27b80323fe106a90f4bb9c1
PauloHASA <paulohasa@gmail.com>
2023-01-24 12:24:20 -0300

    deleting Contato and Sobre from website

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO do App View')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE do App View')
+    return render(request, 'recipes/home.html')
