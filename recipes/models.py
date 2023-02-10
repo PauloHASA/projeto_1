6ccf4bad2f28585e314202810e337f922d580d2b
PauloHASA <paulohasa@gmail.com>
2023-02-10 14:05:06 -0300

    creat fields at data base

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+# Models são tabelas no banco de dados convertido pelo Django.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+# As classes são as tabelas no Banco de dados.
+
+
+class Recipe(models.Model):
+    # Atributos são como colunas na base de dados.
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
